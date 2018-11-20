d16dd92e603e5eb1a0d9c5a2e8f554da6699c052
Karoliina Ranta <kranta@lmos-05800.local.nl.bol.com>
2018-11-20 15:42:11 +0100

    fdd

--- dags/dag4.py
+++ dags/dag4.py
@@ -1,6 +1,7 @@
 import airflow
 from airflow.models import DAG
-from airflow-training-skeleton.airflow_training.hooks.operators.postgres_to_gcs import PostgresToGoogleCloudStorageOperator
+from airflow.contrib.operators.postgres_to_gcs_operator import PostgresToGoogleCloudStorageOperator
+# from airflow-training-skeleton.airflow_training.hooks.operators.postgres_to_gcs import PostgresToGoogleCloudStorageOperator
 
 args = {
     "owner":"karoliina",
