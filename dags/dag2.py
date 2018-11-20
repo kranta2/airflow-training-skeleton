d2fd5b77bb9bfd4b80055e5e8cd902b5745ac330
kranta2 <45197951+kranta2@users.noreply.github.com>
2018-11-20 11:51:50 +0100

    Create dag2.py
    Made exercise 2

---
+++ dags/dag2.py
@@ -0,0 +1,35 @@
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+
+args = {"owner":"karoliina", "start_date":airflow.utils.dates.days_ago(1)}
+
+dag = DAG(
+    dag_id="exercise2",
+    default_args=args,
+)
+
+BashOperator(
+    task_id="print_execution_date", bash_command="echo {{ execution_date }}", dag=dag
+)
+
+BashOperator(
+    task_id="wait_1", bash_commands="sleep 1", dag=dag
+)
+
+BashOperator(
+    task_id="wait_5", bash_commands="sleep 5", dag=dag
+)
+
+BashOperator(
+    task_id="wait_10", bash_commands="sleep 10", dag=dag
+)
+
+DummyOperator(
+    task_id="the_end", dag=dag
+)
+#BashOperator(
+#    task_id="the_end", bash_command="echo {{ execution_date }}", dag=dag
+#)
+
+print_execution_date >> [wait_1, wait_5, wait_10] >> the_end
