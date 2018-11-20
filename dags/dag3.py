35a752e7e3e8991684de3fe07f492655787fc57d
Karoliina Ranta <kranta@lmos-05800.local.nl.bol.com>
2018-11-20 14:29:41 +0100

    fd

--- dags/dag3.py
+++ dags/dag3.py
@@ -27,18 +27,18 @@ dag = DAG(
 )
 
 def p_day(execution_date, **context):
-    print(str(execution_date.day_of_week))
+    print(execution_date.day_of_week)
 
-t1 = PythonOperator(
-    task_id="print_weekday", python_callable=p_day, dag=dag, provide_context=True
-)
-
-# t1 = DummyOperator(
-#     task_id="print_weekday", dag=dag
+# t1 = PythonOperator(
+#     task_id="print_weekday", python_callable=p_day, dag=dag, provide_context=True
 # )
 
+t1 = DummyOperator(
+    task_id="print_weekday", dag=dag
+)
+
 def get_who(execution_date, **context):
-    who = weekday_person_to_email[execution_date.day_of_week]
+    who = weekday_person_to_email[execution_date.day_of_week - 1]
     return "email_".format(str.lower(who))
 
 t2 = BranchPythonOperator(
